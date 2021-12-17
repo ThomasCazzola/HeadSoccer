a5f68949fa8a4b2d124c3c3cc7ce4f095b002f58
danieleoricchio <lele13.do@gmail.com>
2021-12-17 09:52:23 +0100

    Commit movimento e selezione giocatore
    Aggiunta movimento del giocatore a destra e sinistra.
    Aggiunta pagina di selezione del giocatore.

--- HeadSoccer/HeadSoccer/MainWindow.xaml.cs
+++ HeadSoccer/HeadSoccer/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Threading;
 
 namespace HeadSoccer
 {
@@ -20,6 +22,65 @@ namespace HeadSoccer
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        DispatcherTimer timer = new DispatcherTimer();
+        bool goDx, goSx;
+        int gravity = 3;
+        int idPlayer = 0;
+        public MainWindow(int idGiocatore)
+        {
+            InitializeComponent();
+            timer.Tick += new EventHandler(gameEngine);
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Start();
+            idGiocatore = idPlayer;
+            if (idPlayer == 1)
+            {
+                Player.Source = new BitmapImage(new Uri(@"HeadSoccer\\images\\player1.png"));
+            }
+            else if (idPlayer == 2)
+            {
+                Player.Source = new BitmapImage(new Uri(@"HeadSoccer\\images\\player2.png"));
+            }
+        }
+
+        private void gameEngine(object sender, EventArgs e)
+        {
+            if (Canvas.GetTop(Player) > 365)
+            {
+                Canvas.SetTop(Player, Canvas.GetTop(Player) + gravity);
+            }
+            if (goDx)
+            {
+                Canvas.SetLeft(Player, Canvas.GetLeft(Player) + 5);
+            }
+            if (goSx)
+            {
+                Canvas.SetLeft(Player, Canvas.GetLeft(Player) - 5);
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D)
+            {
+                goDx = true;
+            }
+            if (e.Key == Key.A)
+            {
+                goSx = true;
+            }
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.D)
+            {
+                goDx = false;
+            }
+            if (e.Key == Key.A)
+            {
+                goSx = false;
+            }
+        }
     }
 }
--- HeadSoccer/HeadSoccer/obj/Debug/HeadSoccer_Content.g.i.cs
+++ HeadSoccer/HeadSoccer/obj/Debug/HeadSoccer_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("provaimmagine.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("player1.png")]
 
 
