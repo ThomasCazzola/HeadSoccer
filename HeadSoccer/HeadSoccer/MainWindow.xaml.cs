8bb38937031ea2073d13f1ddd72a1b927093aef1
danieleoricchio <lele13.do@gmail.com>
2021-12-30 13:36:14 +0100

    Aggiunte funzionalita
    Aggiunte:
     - Movimento e salto del giocatore
     - Movimento della palla
     - Collisioni giocatore e terreno
     - Collisioni giocatore e palla

--- HeadSoccer/HeadSoccer/MainWindow.xaml.cs
+++ HeadSoccer/HeadSoccer/MainWindow.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
-using System.Threading;
 
 namespace HeadSoccer
 {
@@ -22,45 +12,68 @@ namespace HeadSoccer
     /// </summary>
     public partial class MainWindow : Window
     {
-        DispatcherTimer timer = new DispatcherTimer();
-        bool goDx, goSx;
-        int gravity = 3;
-        int idPlayer = 0;
-        public MainWindow(int idGiocatore)
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        //bool gameOver = false;
+        bool goDx, goSx, intercPalla1; //intercPalla2;
+        int gravity = 8, ballGravity = 5;
+        Rect playerHitBox, ballHitBox, groundHitBox;
+        Random r = new Random();
+        int dirPalla = 0;
+
+        public MainWindow()
         {
             InitializeComponent();
-            timer.Tick += new EventHandler(gameEngine);
-            timer.Interval = TimeSpan.FromMilliseconds(10);
-            timer.Start();
-            idGiocatore = idPlayer;
-            if (idPlayer == 1)
-            {
-                Player.Source = new BitmapImage(new Uri(@"HeadSoccer\\images\\player1.png"));
-            }
-            else if (idPlayer == 2)
-            {
-                Player.Source = new BitmapImage(new Uri(@"HeadSoccer\\images\\player2.png"));
-            }
+            gameTimer.Tick += gameEngine;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(10);
+            StartGame();
+            dirPalla = GeneraDirPallaRandom();
         }
 
         private void gameEngine(object sender, EventArgs e)
         {
-            if (Canvas.GetTop(Player) > 365)
-            {
-                Canvas.SetTop(Player, Canvas.GetTop(Player) + gravity);
-            }
+            playerHitBox = new Rect(Canvas.GetLeft(Player), Canvas.GetTop(Player), Player.ActualWidth, Player.ActualHeight);
+            ballHitBox = new Rect(Canvas.GetLeft(Palla), Canvas.GetTop(Palla), Palla.ActualWidth, Palla.ActualHeight);
+            groundHitBox = new Rect(Canvas.GetLeft(Ground), Canvas.GetTop(Ground), Ground.ActualWidth, Ground.ActualHeight);
+            Canvas.SetTop(Player, Canvas.GetTop(Player) + gravity);
             if (goDx)
             {
-                Canvas.SetLeft(Player, Canvas.GetLeft(Player) + 5);
+                Canvas.SetLeft(Player, Canvas.GetLeft(Player) + 10);
             }
             if (goSx)
             {
                 Canvas.SetLeft(Player, Canvas.GetLeft(Player) - 5);
             }
+            if (!intercPalla1)
+            {
+                if (dirPalla == 1)
+                {
+                    Canvas.SetLeft(Palla, Canvas.GetLeft(Palla) - ballGravity);
+                    Canvas.SetTop(Palla, Canvas.GetTop(Palla) + ballGravity);
+                }
+                else if (dirPalla == 2)
+                {
+                    Canvas.SetLeft(Palla, Canvas.GetLeft(Palla) + ballGravity);
+                    Canvas.SetTop(Palla, Canvas.GetTop(Palla) + ballGravity);
+                }
+            }else if (intercPalla1)
+            {
+                Canvas.SetLeft(Palla, Canvas.GetLeft(Palla) + ballGravity);
+                Canvas.SetTop(Palla, Canvas.GetTop(Palla) - ballGravity);
+            }
+            //else if (intercPalla2){
+            //    Canvas.SetLeft(Palla, Canvas.GetLeft(Palla) - ballGravity);
+            //    Canvas.SetTop(Palla, Canvas.GetTop(Palla) - ballGravity);
+            //}
+
+            Intersezioni();
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
+            if (e.Key == Key.Space)
+            {
+                gravity = 8;
+            }
             if (e.Key == Key.D)
             {
                 goDx = true;
@@ -73,6 +86,10 @@ namespace HeadSoccer
 
         private void Window_KeyUp(object sender, KeyEventArgs e)
         {
+            if (e.Key == Key.Space)
+            {
+                gravity = -8;
+            }
             if (e.Key == Key.D)
             {
                 goDx = false;
@@ -80,6 +97,47 @@ namespace HeadSoccer
             if (e.Key == Key.A)
             {
                 goSx = false;
+            }
+        }
+
+        private void StartGame()
+        {
+            gameTimer.Start();
+            myCanvas.Focus();
+            //gameOver = false;
+            Canvas.SetTop(Player, 600);
+            Canvas.SetLeft(Player, 200);
+        }
+
+        private void EndGame()
+        {
+            //gameOver = true;
+        }
+
+        private int GeneraDirPallaRandom()
+        {
+            dirPalla = r.Next(2) + 1;
+            return dirPalla;
+        }
+
+        private void Intersezioni()
+        {
+            if (playerHitBox.IntersectsWith(groundHitBox))
+            {
+                Canvas.SetTop(Player, Canvas.GetTop(Ground) - Player.ActualHeight);
+            }
+            if (ballHitBox.IntersectsWith(playerHitBox))
+            {
+                if (dirPalla == 1)
+                { 
+                    intercPalla1 = true;
+                    dirPalla = 2;
+                }
+                //if(dirPalla == 2)
+                //{
+                //    intercPalla2 = true;
+                //    dirPalla = 1;
+                //}
             }
         }
     }
