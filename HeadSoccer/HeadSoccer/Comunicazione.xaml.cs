e5bae498a2d98bf3db67de7ce4a3bc0484174216
danieleoricchio <lele13.do@gmail.com>
2022-01-06 17:35:26 +0100

    Comunicazione peer to peer
    Creata la classe per la comunicazione tra i due client.
    La classe contiene due metodi per l'invio e la ricezioni dei messaggi.
    Ho inoltre creato la pagina iniziale per il collegamento tra i due client.

---
+++ HeadSoccer/HeadSoccer/Comunicazione.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HeadSoccer
+{
+    /// <summary>
+    /// Interaction logic for Comunicazione.xaml
+    /// </summary>
+    public partial class Comunicazione : Window
+    {
+        ComunicazionePlayer comPlayer = null;
+        string ipDest = string.Empty;
+        public Comunicazione()
+        {
+            InitializeComponent();
+            ipDest = txtIp.Text;
+        }
+
+        private void btnInvia_Click(object sender, RoutedEventArgs e)
+        {
+           comPlayer = new ComunicazionePlayer();
+           comPlayer.Send("c;" + txtNome.Text, ipDest);
+        }
+    }
+}
---
+++ HeadSoccer/HeadSoccer/ComunicazionePlayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadSoccer
+{
+    class ComunicazionePlayer
+    {
+        private UdpClient client;
+        public void Send(string dataIn, string ipAddress)
+        {
+            client = new UdpClient();
+
+            byte[] data = Encoding.ASCII.GetBytes(dataIn);
+
+            client.Send(data, data.Length, ipAddress, 12345);
+        }
+
+        public string Receive()
+        {
+            IPEndPoint riceveEP = new IPEndPoint(IPAddress.Any, 0);
+
+            byte[] dataReceived = client.Receive(ref riceveEP);
+
+            string risposta = Encoding.ASCII.GetString(dataReceived);
+
+            return risposta;
+        }
+    }
+}
