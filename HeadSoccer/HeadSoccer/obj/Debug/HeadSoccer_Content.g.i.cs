99604d661063b90db46c991399542c9934060a64
danieleoricchio <lele13.do@gmail.com>
2022-01-12 20:40:54 +0100

    Abbellimento grafica Window
    - Abbellite le window Comunicazione.xaml e SelectPlayer.xaml aggiungendo il logo di headSoccer.

--- HeadSoccer/HeadSoccer/obj/Debug/HeadSoccer_Content.g.i.cs
+++ HeadSoccer/HeadSoccer/obj/Debug/HeadSoccer_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/lato.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/logohead.png")]
 
 
